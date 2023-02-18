7b5b6d3850563ceaa27b3f6436092b63674305a9
Zebreus <lennarteichhorn@googlemail.com>
2023-02-19 00:26:16 +0100

    Add scroll restoration

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env["NEXT_PUBLIC_GITHUB_PAGES_BASE"] || "",
   assetPrefix: (process.env["NEXT_PUBLIC_GITHUB_PAGES_BASE"] || "") + "/",
+  experimental: {
+    scrollRestoration: true,
+  },
 }
 
 export default withSuperjson()(nextConfig)
