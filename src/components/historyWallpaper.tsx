bc0cb624029a4d207dc0a55c2c98bef6f672902d
Lennart <lennarteichhorn@googlemail.com>
2023-02-18 01:31:12 +0000

    Try to fix links

--- src/components/historyWallpaper.tsx
+++ src/components/historyWallpaper.tsx
@@ -2,55 +2,76 @@ import { css } from "@emotion/react"
 import { getCssFit } from "functions/getCssFit"
 import { getImageUrl } from "functions/getImageUrl"
 import { Snapshot } from "functions/processData"
-import Link from "next/link"
 
 type HistoryWallpaperProps = { snapshot: Required<Snapshot> }
 
 export const HistoryWallpaper = ({ snapshot }: HistoryWallpaperProps) => {
   return (
-    <Link href={"../" + getImageUrl(snapshot.url)} passHref>
-      <a
+    <a
+      css={css`
+        margin-left: 2rem;
+        margin-right: 2rem;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        display: flex;
+        @media (max-width: 1000px) {
+          flex-direction: column;
+          margin-left: 5vw;
+          margin-right: 5vw;
+          margin-top: 1rem;
+          margin-bottom: 1rem;
+        }
+      `}
+      href={"../" + getImageUrl(snapshot.url)}
+    >
+      <img
         css={css`
-          margin-left: 2rem;
-          margin-right: 2rem;
-          margin-top: 2rem;
-          margin-bottom: 2rem;
-          display: flex;
+          border-radius: 2rem;
+          width: 75vw;
+          max-width: 1000px;
           @media (max-width: 1000px) {
+            border-radius: 5vw;
+
+            width: auto;
+          }
+          height: auto;
+          //"stretched" | "zoom" | "scaled" | "wallpaper" | "fill"
+
+          object-fit: ${getCssFit(snapshot.fillMode)};
+          background-color: ${snapshot.primaryColor};
+          cursor: pointer;
+        `}
+        src={"../" + getImageUrl(snapshot.url)}
+        alt={snapshot.name}
+      />
+      <section
+        css={css`
+          width: auto;
+          margin: 1rem;
+          margin-top: 1rem;
+          @media (max-width: 1000px) {
+            order: -1;
+            display: flex;
             flex-direction: column;
-            margin-left: 5vw;
-            margin-right: 5vw;
-            margin-top: 1rem;
-            margin-bottom: 1rem;
+            flex-basis: center;
+            justify-content: center;
+            align-items: center;
           }
         `}
       >
-        <img
+        <h2
           css={css`
-            border-radius: 2rem;
-            width: 75vw;
-            max-width: 1000px;
-            @media (max-width: 1000px) {
-              border-radius: 5vw;
-
-              width: auto;
-            }
-            height: auto;
-            //"stretched" | "zoom" | "scaled" | "wallpaper" | "fill"
-
-            object-fit: ${getCssFit(snapshot.fillMode)};
-            background-color: ${snapshot.primaryColor};
-            cursor: pointer;
-          `}
-          src={"../" + getImageUrl(snapshot.url)}
-          alt={snapshot.name}
-        />
-        <section
-          css={css`
-            width: auto;
-            margin: 1rem;
+            font-size: 3rem;
+            margin-bottom: 0;
             margin-top: 1rem;
+            display: flex;
+            flex-direction: column;
+            flex-basis: center;
+            justify-content: center;
             @media (max-width: 1000px) {
+              font-size: 1.5rem;
+              margin-bottom: 0;
+              margin-top: 0rem;
               order: -1;
               display: flex;
               flex-direction: column;
@@ -59,41 +80,18 @@ export const HistoryWallpaper = ({ snapshot }: HistoryWallpaperProps) => {
               align-items: center;
             }
           `}
+          title={"Commit: " + snapshot?.message}
         >
-          <h2
-            css={css`
-              font-size: 3rem;
-              margin-bottom: 0;
-              margin-top: 1rem;
-              display: flex;
-              flex-direction: column;
-              flex-basis: center;
-              justify-content: center;
-              @media (max-width: 1000px) {
-                font-size: 1.5rem;
-                margin-bottom: 0;
-                margin-top: 0rem;
-                order: -1;
-                display: flex;
-                flex-direction: column;
-                flex-basis: center;
-                justify-content: center;
-                align-items: center;
-              }
-            `}
-            title={"Commit: " + snapshot?.message}
-          >
-            {snapshot.date.toLocaleDateString("de")}
-          </h2>
-          <span
-            css={css`
-              font-size: 1rem;
-            `}
-          >
-            {snapshot?.message}
-          </span>
-        </section>
-      </a>
-    </Link>
+          {snapshot.date.toLocaleDateString("de")}
+        </h2>
+        <span
+          css={css`
+            font-size: 1rem;
+          `}
+        >
+          {snapshot?.message}
+        </span>
+      </section>
+    </a>
   )
 }
--- src/components/overviewWallpaper.tsx
+++ src/components/overviewWallpaper.tsx
@@ -3,7 +3,6 @@ import { fixDisplayName } from "functions/fixDisplayName"
 import { getCssFit } from "functions/getCssFit"
 import { getThumbnailUrl } from "functions/getImageUrl"
 import { WallpaperWithHistory } from "functions/processData"
-import Link from "next/link"
 
 export type OverviewProps = {
   wallpaper: WallpaperWithHistory
@@ -14,86 +13,86 @@ export const OverviewWallpaper = ({ wallpaper }: OverviewProps) => {
   if (!latestSnapshot) throw new Error("No latest snapshot")
 
   return (
-    <Link href={`wallpaper/${latestSnapshot.hash}`} passHref key={latestSnapshot.name}>
-      <a
+    <a
+      css={css`
+        margin: 1rem;
+        width: auto;
+        height: fit-content;
+        display: flex;
+        flex-direction: column;
+      `}
+      href={`wallpaper/${latestSnapshot.hash}`}
+      key={latestSnapshot.name}
+    >
+      <img
         css={css`
-          margin: 1rem;
+          border-radius: 1rem;
+          width: 100%;
+          height: 309px;
+          //"stretched" | "zoom" | "scaled" | "wallpaper" | "fill"
+
+          object-fit: ${getCssFit(latestSnapshot.fillMode)};
+          background-color: ${latestSnapshot.primaryColor};
+        `}
+        src={getThumbnailUrl(latestSnapshot.url)}
+        alt={latestSnapshot.name}
+      />
+      <section
+        css={css`
           width: auto;
-          height: fit-content;
-          display: flex;
-          flex-direction: column;
+          margin: 0;
+          margin-top: 0;
+          @media (max-width: 700px) {
+            order: -1;
+            display: flex;
+            flex-direction: row;
+            flex-basis: center;
+            justify-content: center;
+            align-items: baseline;
+            row-gap: 1rem;
+            gap: 1rem;
+          }
         `}
       >
-        <img
+        <h2
           css={css`
-            border-radius: 1rem;
-            width: 100%;
-            height: 309px;
-            //"stretched" | "zoom" | "scaled" | "wallpaper" | "fill"
-
-            object-fit: ${getCssFit(latestSnapshot.fillMode)};
-            background-color: ${latestSnapshot.primaryColor};
-          `}
-          src={getThumbnailUrl(latestSnapshot.url)}
-          alt={latestSnapshot.name}
-        />
-        <section
-          css={css`
-            width: auto;
-            margin: 0;
+            font-size: 2rem;
+            color: white;
+            text-decoration: none !important;
+            cursor: inherit;
             margin-top: 0;
-            @media (max-width: 700px) {
-              order: -1;
-              display: flex;
-              flex-direction: row;
-              flex-basis: center;
-              justify-content: center;
-              align-items: baseline;
-              row-gap: 1rem;
-              gap: 1rem;
-            }
+            margin-bottom: 0;
           `}
         >
-          <h2
+          {fixDisplayName(latestSnapshot.name)}
+        </h2>
+        <span>
+          <span
             css={css`
-              font-size: 2rem;
-              color: white;
-              text-decoration: none !important;
-              cursor: inherit;
-              margin-top: 0;
-              margin-bottom: 0;
+              font-size: 1rem;
             `}
+            title={"Commit: " + latestSnapshot?.message}
           >
-            {fixDisplayName(latestSnapshot.name)}
-          </h2>
-          <span>
-            <span
-              css={css`
-                font-size: 1rem;
-              `}
-              title={"Commit: " + latestSnapshot?.message}
-            >
-              {wallpaper.created.toLocaleDateString("de")}
-            </span>
-            {" - "}
-            <span
-              css={css`
-                font-size: 1rem;
-              `}
-              title={"Commit: " + wallpaper.deleteMessage}
-            >
-              {wallpaper.deleted.toLocaleDateString("de")}
-            </span>
+            {wallpaper.created.toLocaleDateString("de")}
           </span>
-          <section>
-            {latestSnapshot.dark ? <span>Dark</span> : null}
-            {latestSnapshot.night ? <span>Night</span> : null}
-            {latestSnapshot.day ? <span>Day</span> : null}
-            {latestSnapshot.morning ? <span>Morning</span> : null}
-            {latestSnapshot.alt ? <span>Alternative</span> : null}
-          </section>
+          {" - "}
+          <span
+            css={css`
+              font-size: 1rem;
+            `}
+            title={"Commit: " + wallpaper.deleteMessage}
+          >
+            {wallpaper.deleted.toLocaleDateString("de")}
+          </span>
+        </span>
+        <section>
+          {latestSnapshot.dark ? <span>Dark</span> : null}
+          {latestSnapshot.night ? <span>Night</span> : null}
+          {latestSnapshot.day ? <span>Day</span> : null}
+          {latestSnapshot.morning ? <span>Morning</span> : null}
+          {latestSnapshot.alt ? <span>Alternative</span> : null}
         </section>
-      </a>
-    </Link>
+      </section>
+    </a>
   )
 }
