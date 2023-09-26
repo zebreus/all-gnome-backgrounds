d9a273ec6f504da19ef8728c253de98b4448d47f
Zebreus <lennarteichhorn@googlemail.com>
2023-09-26 21:44:14 +0200

    Update libjxl to support current images

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "All GNOME backgrounds";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
-    flake-utils.url = "github:numtide/flake-utils"; 
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils}:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       rec {
         name = "all-gnome-backgrounds";
